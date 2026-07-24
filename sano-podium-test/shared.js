673e4fa5949c75538316014dfde23a58e9bbde70
jasper4-web <jaspersmind4@icloud.com>
2026-07-23 18:18:48 -0700

    Round 8: pre-render 8 industry pages as real static HTML (own title/description/canonical, works without JS); correct FTC authority; hedge service levels; per-item tier truth; SMS consent links; vendor conflicts in article refs

--- sano-podium-test/shared.js
+++ sano-podium-test/shared.js
@@ -7,7 +7,7 @@
   var IND = window.SANO_INDUSTRIES || [];
 
   var indDrop = IND.map(function (i) {
-    return '<a href="industry.html?ind=' + i.slug + '"><b>' + i.label + '</b><span>' + i.tag + '</span></a>';
+    return '<a href="industry-' + i.slug + '.html"><b>' + i.label + '</b><span>' + i.tag + '</span></a>';
   }).join('');
 
   var caret = '<svg class="caret" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><polyline points="6 9 12 15 18 9"/></svg>';
@@ -52,7 +52,7 @@
     '</div>';
 
   var indFoot = IND.slice(0, 4).map(function (i) {
-    return '<a href="industry.html?ind=' + i.slug + '">' + i.label + '</a>';
+    return '<a href="industry-' + i.slug + '.html">' + i.label + '</a>';
   }).join('') + '<a href="industries.html">All industries →</a>';
 
   var foot =
